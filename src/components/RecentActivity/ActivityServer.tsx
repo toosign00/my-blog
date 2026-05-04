f460f286c91775e0e84319879867719ab830f4dd
toosign <kevinsoo1014@gmail.com>
2026-05-05 03:26:12 +0900

    fix: graceful degradation on GitHub API failure in ActivityServer

--- src/components/RecentActivity/ActivityServer.tsx
+++ src/components/RecentActivity/ActivityServer.tsx
@@ -1,7 +1,12 @@
-import { fetchRecentGitHubActivities } from '@/utils/github-activity-util';
+import { type ActivityItem, fetchRecentGitHubActivities } from '@/utils/github-activity-util';
 import { RecentActivity } from './index';
 
 export const ActivityServer = async () => {
-  const activities = await fetchRecentGitHubActivities();
+  let activities: ActivityItem[] = [];
+  try {
+    activities = await fetchRecentGitHubActivities();
+  } catch {
+    // GitHub API 실패 시 빈 목록으로 graceful degradation
+  }
   return <RecentActivity initialActivities={activities} />;
 };
